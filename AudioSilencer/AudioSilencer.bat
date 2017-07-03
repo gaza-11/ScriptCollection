3b92f502a6212ec2bb3b9081f5179a0d89f24686
goza-11 <muddy.brain29@gmail.com>
2017-07-03 21:27:21 +0900

    ログファイル出力するよう修正

--- AudioSilencer/AudioSilencer.bat
+++ AudioSilencer/AudioSilencer.bat
@@ -1,35 +1,48 @@
-
+@echo off
 setlocal enabledelayedexpansion
 rem *****************************************************************
 rem *** Windows消音/消音取消処理
 rem *** 引数：（１）1＝消音・2＝消音取消・省略した場合は2と同じ
 rem *****************************************************************
 
+set LOG_DIR=%~dp0
+set LOG_FILE=%LOG_DIR%\%~n0.log
+
 set SV_NAME1=Audiosrv
 set SV_NAME2=AudioEndpointBuilder
 
+echo %DATE% %TIME% [Info ] 処理開始 > %LOG_FILE%
 set MODE=%1
 
 if "%MODE%"=="1" (
-    net stop %SV_NAME1%
+    echo %DATE% %TIME% [Info ] 消音モードで実行 >> %LOG_FILE%
+
+    net stop %SV_NAME1% >> %LOG_FILE% 2>&1
     call :CHECK_STATE %SV_NAME1%
     if ERRORLEVEL 1 goto ERROR
+    echo %DATE% %TIME% [Info ] %SV_NAME1%終了完了>> %LOG_FILE%
 
-    net stop %SV_NAME2%
+    net stop %SV_NAME2% >> %LOG_FILE% 2>&1
     call :CHECK_STATE %SV_NAME2%
     if ERRORLEVEL 1 goto ERROR
+    echo %DATE% %TIME% [Info ] %SV_NAME2%終了完了>> %LOG_FILE%
 ) else (
-    net start %SV_NAME2%
+    echo %DATE% %TIME% [Info ] 消音取消モードで実行 >> %LOG_FILE%
+
+    net start %SV_NAME2% >> %LOG_FILE% 2>&1
     call :CHECK_STATE %SV_NAME2%
     if !ERRORLEVEL!==0 goto ERROR
+    echo %DATE% %TIME% [Info ] %SV_NAME2%開始完了>> %LOG_FILE%
 
-    net start %SV_NAME1%
+    net start %SV_NAME1% >> %LOG_FILE% 2>&1
     call :CHECK_STATE %SV_NAME1%
     if !ERRORLEVEL!==0 goto ERROR
+    echo %DATE% %TIME% [Info ] %SV_NAME1%開始完了>> %LOG_FILE%
 )
 
-echo success
-pause
+echo %DATE% %TIME% [Info ] 処理完了 >> %LOG_FILE%
+
+endlocal
 exit /b 0
 
 :CHECK_STATE
@@ -37,6 +50,7 @@ sc query %1 | findstr STOPPED
 exit /b %ERRORLEVEL%
 
 :ERROR
-echo error
-pause
+echo %DATE% %TIME% [Error] エラー発生。終了コード：%ERRORLEVEL% >> %LOG_FILE%
+
+endlocal
 exit /b %ERRORLEVEL%
